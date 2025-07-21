881af9ac2c7b0289f0b49d2e7f63d427b455e7ce
Alexpena76 <alexpenasilva@gmail.com>
2025-07-21 16:29:01 -0500

    Q10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a pyramid from right to left
+ * @size: size of the bottom line
+ *
+ * Return: void function
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
