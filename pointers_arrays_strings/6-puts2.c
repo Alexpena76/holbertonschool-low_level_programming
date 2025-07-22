64d96556ebd69cd2f4df2dc95cf569eca40ab3a9
Alexpena76 <alexpenasilva@gmail.com>
2025-07-22 08:19:47 -0500

    Q6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,25 @@
 #include <string.h>
 
 /**
+ * _strlen - gives the length of a string
+ * @s: string
+ *
+ * Return: the length of the string
+ */
+
+
+int _strlen(char *s)
+{
+	int contador = 0;
+
+	while (s[contador] != '\0')
+	{
+		contador++;
+	}
+	return (contador);
+}
+
+/**
  * puts2 - prints every other character of a string
  * @str: string
  *
@@ -12,9 +31,7 @@ void puts2(char *str)
 {
 	int i;
 
-	int m = strlen(str);
-
-	for (i = 0; i <= m; i += 2)
+	for (i = 0; i <= _strlen(str); i += 2)
 	{
 		_putchar(str[i]);
 	}
