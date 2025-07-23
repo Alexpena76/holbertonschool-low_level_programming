600a80ccdf8201828dd033d75a08bb9d21468747
Alexpena76 <alexpenasilva@gmail.com>
2025-07-23 17:53:00 -0500

    Q1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: First operand
+ * @src: Second operand
+ * @n: Size
+ *
+ * Return: The concatenation of the two strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	int j = 0;
+	int count = 0;
+
+	while (count < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		count++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
