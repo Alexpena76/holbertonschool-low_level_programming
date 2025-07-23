b2f73c023b61dd7cfc80c24a11c87e87db1955a6
Alexpena76 <alexpenasilva@gmail.com>
2025-07-23 17:10:14 -0500

    Q10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,50 @@
 #include "main.h"
 
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to be printed
+ *
+ * Return: Digits of the string
+ */
+
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-    int found_digit = 0;
-    int digit_value;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+	int found_digit = 0;
+	int digit_value = 0;
 
-    while (s[i] != '\0' && (!(s[i] >= '0' && s[i] <= '9')) && s[i] != '+' && s[i] != '-')
-    {
-        i++;
-    }
-    
-    while (s[i] != '\0' && (s[i] == '+' || s[i] == '-'))
-    {
-        if (s[i] == '-')
-        {
-            sign = sign * -1;
-        }
-        i++;
-    } 
+	while (s[i] != '\0' && (!(s[i] >= '0' && s[i] <= '9'))
+		&& s[i] != '+' && s[i] != '-')
+	{
+			i++;
+	}
 
-    while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-    {
-        found_digit = 1;
+	while (s[i] != '\0' && (s[i] == '+' || s[i] == '-'))
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		i++;
+	}
 
-        digit_value = s[i] - '0';
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	{
+		found_digit = 1;
 
-        result = result * 10 + digit_value;
+		digit_value = s[i] - '0';
 
-        i++;
-    }
+		result = result * 10 + digit_value;
 
-    if (found_digit == 0)
-    {
-        return 0;
-    }
-    else
-        return result * sign;
+		i++;
+	}
+
+	if (found_digit == 0)
+	{
+		return (0);
+	}
+	else
+		return (result * sign);
 }
