336f05e6e39c3ef1412472b40455d0a14914d23c
Alexpena76 <alexpenasilva@gmail.com>
2025-07-24 12:21:04 -0500

    Q0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
+/**
+ * _memset - Copies a value onto the first operand n times
+ * @s: Pointer to the memory area to be filled
+ * @b: Value to be copied
+ * @n: Amount of times the value will be copied
+ *
+ * Return: The new array
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	int i = 0;
 
-	while (n > 0)
+	while (i < n)
 	{
-		*ptr = b;
-		ptr++;
-		n--;
+		s[i] = b;
+		i++;
 	}
 
 	return (s);
