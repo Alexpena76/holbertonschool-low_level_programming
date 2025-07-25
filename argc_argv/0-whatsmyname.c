0ec333d14fb8947e1b891b8efb8a334327c5b1f5
Alexpena76 <alexpenasilva@gmail.com>
2025-07-25 15:28:07 -0500

    Q0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <stdio.h>
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
+    (void)argc;
     printf("%s", argv[0]);
     printf("\n");
     return (0);
