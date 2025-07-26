cc6a8ee2d2eb38dddd5901e40dce01607f659b35
Alexpena76 <alexpenasilva@gmail.com>
2025-07-26 08:43:48 -0500

    Q3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Multiplies two numbers passed as command line arguments
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Return: 0 on success, 1 if wrong arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		printf("\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d", result);
+
+	printf("\n");
+
+	return (0);
+
+}
