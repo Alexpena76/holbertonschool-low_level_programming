d08dd4e7c8045462da7f6e9903a03e2c5a361491
Alexpena76 <alexpenasilva@gmail.com>
2025-07-26 09:38:06 -0500

    Q4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main- Adds positive numbers passed as command line arguments
+ * @argc: Number of command line arguments
+ * @argv: Array of command line argument strings
+ *
+ * Return: 0 on success, 1 if invalid input is found
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 0;
+	int j = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
