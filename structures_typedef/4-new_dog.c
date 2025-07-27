c41f2787707e36511162b9c9f98726c2c3eb7e5a
Alexpena76 <alexpenasilva@gmail.com>
2025-07-27 14:43:42 -0500

    Q4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the newly created dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog_ptr;
+	int name_length;
+	int owner_length;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog_ptr = malloc(sizeof(dog_t));
+
+	if (new_dog_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	name_length = strlen(name) + 1;
+	owner_length = strlen(owner) + 1;
+
+	new_dog_ptr->name = malloc(name_length);
+
+	if (new_dog_ptr->name == NULL)
+	{
+		free(new_dog_ptr);
+		return (NULL);
+	}
+
+	new_dog_ptr->owner = malloc(owner_length);
+
+	if (new_dog_ptr->owner == NULL)
+	{
+		free(new_dog_ptr->name);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+
+	strcpy(new_dog_ptr->name, name);
+	strcpy(new_dog_ptr->owner, owner);
+
+	new_dog_ptr->age = age;
+
+	return (new_dog_ptr);
+}
