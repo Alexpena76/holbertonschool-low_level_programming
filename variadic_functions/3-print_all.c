8251b40eccf408e1f12087614c44327f8c5fffc3
Alexpena76 <alexpenasilva@gmail.com>
2025-07-29 12:05:37 -0500

    Q3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything based on format specifiers
+ * @format: list of types of arguments passed to the function
+ *          c: char
+ *          i: integer  
+ *          f: float
+ *          s: char * (prints (nil) if string is NULL)
+ *          any other char is ignored
+ * 
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *separator = "";
+	char current_char;
+	char char_arg;
+	int int_arg = 0;
+	float float_arg = 0;
+	char *string_arg;
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		current_char = format[i];
+
+		if (current_char == 'c' || current_char == 'i' || current_char == 'f' || current_char == 's')
+		{
+			printf("%s", separator);
+
+			while (1)
+			{
+				if (current_char == 'c')
+				{
+					char_arg = va_arg(args, int);
+					printf("%c", char_arg);
+					break;
+				}
+
+				 if (current_char == 'i')
+				{
+					int_arg = va_arg(args, int);
+					printf("%d", int_arg);
+					break;
+				}
+
+				if (current_char == 'f')
+				{
+					float_arg = va_arg(args, double);
+					printf("%f", float_arg);
+					break;
+				}
+
+				if (current_char == 's')
+				{
+					string_arg = va_arg(args, char*);
+					if (string_arg == NULL)
+					{
+						printf("(nil)");
+					}
+
+					if (string_arg != NULL)
+					{
+						printf("%s", string_arg);
+					}
+					break;
+				}
+			}
+
+			separator = ", ";
+		}
+
+		i = i + 1;
+	}
+
+	va_end(args);
+	printf("\n");
+}
