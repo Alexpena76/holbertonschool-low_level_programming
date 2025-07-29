34f447e92d56f5153685a7443fa40c952c10e778
Alexpena76 <alexpenasilva@gmail.com>
2025-07-29 12:09:48 -0500

    Q3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
+/*
  * print_all - prints anything based on format specifiers
  * @format: list of types of arguments passed to the function
  *          c: char
- *          i: integer  
+ *          i: integer
  *          f: float
  *          s: char * (prints (nil) if string is NULL)
  *          any other char is ignored
- * 
+ *
  * Return: void
  */
 
@@ -21,69 +21,65 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 	char current_char;
 	char char_arg;
-	int int_arg = 0;
-	float float_arg = 0;
+	int int_arg;
+	float float_arg;
 	char *string_arg;
-
-	if (format == NULL)
-	{
-		printf("\n");
-		return;
-	}
 
 	va_start(args, format);
 
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')  // Remove NULL check if
 	{
 		current_char = format[i];
 
+		// IF #1: Check if valid type
 		if (current_char == 'c' || current_char == 'i' || current_char == 'f' || current_char == 's')
 		{
 			printf("%s", separator);
 
 			while (1)
 			{
-				if (current_char == 'c')
+				// Use switch-like logic with while/break instead of multiple ifs
+				while (current_char == 'c')
 				{
 					char_arg = va_arg(args, int);
 					printf("%c", char_arg);
 					break;
 				}
 
-				 if (current_char == 'i')
+				while (current_char == 'i')
 				{
 					int_arg = va_arg(args, int);
 					printf("%d", int_arg);
 					break;
 				}
 
-				if (current_char == 'f')
+				while (current_char == 'f')
 				{
 					float_arg = va_arg(args, double);
 					printf("%f", float_arg);
 					break;
 				}
 
-				if (current_char == 's')
+				while (current_char == 's')
 				{
 					string_arg = va_arg(args, char*);
+					// IF #2: Handle NULL string
 					if (string_arg == NULL)
-					{
 						printf("(nil)");
-					}
-
-					if (string_arg != NULL)
+					while (string_arg != NULL)  // Use while instead of if
 					{
 						printf("%s", string_arg);
+						break;
 					}
 					break;
 				}
+				break;  // Exit the outer while(1)
 			}
 
 			separator = ", ";
 		}
 
-		i = i + 1;
+		i++;
 	}
 
 	va_end(args);
