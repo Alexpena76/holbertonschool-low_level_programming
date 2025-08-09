9577e05376cb94a05e91b981cf48e7c356196f59
Alexpena76 <alexpenasilva@gmail.com>
2025-08-09 11:12:35 -0500

    Switched out sprintf

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,100 +1,125 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 #define BUFFER_SIZE 1024
 
 /**
-* close_file - Closes a file descriptor with error handling
-* @fd: File descriptor to close
-*
-* Return: Void function
-*/
-
-void close_file(int fd)
+ * write_string - Writes a string to file descriptor
+ * @fd: File descriptor to write to
+ * @str: String to write
+ */
+void write_string(int fd, char *str)
 {
-	char error_msg[100];
-	int len;
-	
-	if (close(fd) == -1)
-	{
-		len = sprintf(error_msg, "Error: Can't close fd %d\n", fd);
-        write(STDERR_FILENO, error_msg, len);
-		exit(100);
-	}
+    int len = 0;
+    
+    /* Calculate string length manually */
+    while (str[len])
+        len++;
+    
+    write(fd, str, len);
 }
 
 /**
- * main - Copies the content of a file to another file
- * @argc: Number of command line arguments
- * @argv: Array of command line arguments
- *
- * Return: 0 on success, or exit with specific error codes on failure
+ * write_number - Writes a number to file descriptor
+ * @fd: File descriptor to write to  
+ * @num: Number to write
  */
+void write_number(int fd, int num)
+{
+    char digits[20];
+    int i = 0, j;
+    
+    if (num == 0)
+    {
+        write(fd, "0", 1);
+        return;
+    }
+    
+    /* Convert number to string manually */
+    while (num > 0)
+    {
+        digits[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+    
+    /* Write digits in reverse order */
+    for (j = i - 1; j >= 0; j--)
+        write(fd, &digits[j], 1);
+}
 
- int main(int argc, char *argv[])
- {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-	char error_msg[200];
-	int len;
+void close_file(int fd)
+{
+    if (close(fd) == -1)
+    {
+        write_string(STDERR_FILENO, "Error: Can't close fd ");
+        write_number(STDERR_FILENO, fd);
+        write_string(STDERR_FILENO, "\n");
+        exit(100);
+    }
+}
 
-	if (argc != 3)
-	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-		exit(97);
-	}
+int main(int argc, char *argv[])
+{
+    int fd_from, fd_to;
+    ssize_t bytes_read, bytes_written;
+    char buffer[BUFFER_SIZE];
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		len = sprintf(error_msg, "Error: Can't read from file %s\n", argv[1]);
-        write(STDERR_FILENO, error_msg, len);
-		exit(98);
-	}
+    if (argc != 3)
+    {
+        write_string(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		len = sprintf(error_msg, "Error: Can't write to %s\n", argv[2]);
-        write(STDERR_FILENO, error_msg, len);
-		close_file(fd_from);
-		exit(99);
-	}
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        write_string(STDERR_FILENO, "Error: Can't read from file ");
+        write_string(STDERR_FILENO, argv[1]);
+        write_string(STDERR_FILENO, "\n");
+        exit(98);
+    }
 
-	while (1)
-	{
-		bytes_read = read(fd_from, buffer, 1024);
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        write_string(STDERR_FILENO, "Error: Can't write to ");
+        write_string(STDERR_FILENO, argv[2]);
+        write_string(STDERR_FILENO, "\n");
+        close_file(fd_from);
+        exit(99);
+    }
 
-		if (bytes_read == 0)
-		{
-			break;
-		}
+    /* Copy file content in chunks */
+    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written == -1 || bytes_written != bytes_read)
+        {
+            write_string(STDERR_FILENO, "Error: Can't write to ");
+            write_string(STDERR_FILENO, argv[2]);
+            write_string(STDERR_FILENO, "\n");
+            close_file(fd_from);
+            close_file(fd_to);
+            exit(99);
+        }
+    }
 
-		if (bytes_read == -1)
-		{
-			len = sprintf(error_msg, "Error: Can't read from file %s\n", argv[1]);
-        	write(STDERR_FILENO, error_msg, len);
-			close_file(fd_from);
-			close_file(fd_to);
-			exit(98);
-		}
+    /* Check for read error */
+    if (bytes_read == -1)
+    {
+        write_string(STDERR_FILENO, "Error: Can't read from file ");
+        write_string(STDERR_FILENO, argv[1]);
+        write_string(STDERR_FILENO, "\n");
+        close_file(fd_from);
+        close_file(fd_to);
+        exit(98);
+    }
 
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			len = sprintf(error_msg, "Error: Can't write to %s\n", argv[2]);
-        	write(STDERR_FILENO, error_msg, len);
-			close_file(fd_from);
-			close_file(fd_to);
-			exit(99);
-		}
-	}
+    /* Close both files */
+    close_file(fd_from);
+    close_file(fd_to);
 
-	close_file(fd_from);
-	close_file(fd_to);
+    return (0);
+}
 
-	return (0);
- }
