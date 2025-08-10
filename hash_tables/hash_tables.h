133f8c8a86b5d59f4d7a15e58c5e6ec7bbbcd699
Alexpena76 <alexpenasilva@gmail.com>
2025-08-10 13:30:28 -0500

    Removed duplicated struct

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -30,20 +30,6 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
-
-typedef struct hash_node_s
-{
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
